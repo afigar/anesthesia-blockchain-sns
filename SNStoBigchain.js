896de43fa64957f11791be6c72f8ae6e0514ccc4
Alejandro Figar Gutierrez <afigar@gmail.com>
2019-09-12 15:12:00 -0300

    Initial commit

---
+++ SNStoBigchain.js
@@ -0,0 +1,56 @@
+'use strict';
+const driver = require('bigchaindb-driver');
+const serverless = require('serverless-http');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+const alice = new driver.Ed25519Keypair();
+const conn = new driver.Connection('http://test-bigchaindb.for-our.info:9984/api/v1/');
+
+function addslashes(string) {
+  return string.replace(/\\/g, '\\\\').
+      replace(/\u0008/g, '\\b').
+      replace(/\t/g, '\\t').
+      replace(/\n/g, '\\n').
+      replace(/\f/g, '\\f').
+      replace(/\r/g, '\\r').
+      replace(/'/g, '\\\'').
+      replace(/"/g, '\\"');
+};
+
+module.exports.handler = async (event) => {
+  const auxmessage = event.Records[0].Sns.Message;
+  console.log('Message received from SNS:', auxmessage); 
+  const tx = driver.Transaction.makeCreateTransaction(
+/*     {
+      datetime: new Date().toString(),
+      location: 'Maternity',
+      or: 'q11',
+      device: {
+          model: 'Philips Intellivue MP40',
+          id: '1234asbdfsg',
+      }
+    }  ,
+ */ 
+   { datetime: new Date().toString(),
+      location: 'Maternity',
+      or: 'q11',
+      device: {
+          model: 'Philips Intellivue MP40',
+          id: '1234asbdfsg',
+      },
+      message: auxmessage
+    },
+    [ driver.Transaction.makeOutput(
+        driver.Transaction.makeEd25519Condition(alice.publicKey))],
+    alice.publicKey)
+      const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
+      await conn.postTransactionCommit(txSigned).then(function(retrievedTx){
+      },function(err){
+        console.log(err)
+      });
+  const result = txSigned
+  console.log(txSigned)
+  return result
+}
+
